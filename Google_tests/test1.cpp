ca2a799ae9a3e6775804e396d9c9b83dbf2b0821
Name <Email>
2022-11-18 13:50:10 +0000

    three tests and cmakefile changed for gmock

--- Google_tests/test1.cpp
+++ Google_tests/test1.cpp
@@ -1,5 +1,41 @@
-#include<gtest/gtest.h>
-TEST(aaa,bb)
+#include <gtest/gtest.h>
+#include<iostream>
+class MyClass
+{   
+    int baseVal;
+public:
+    MyClass(int _bv) : baseVal(_bv) {}
+    void increment (int byValue)
+    {
+        baseVal+=byValue;
+    }
+    int getValue() {return baseVal;}
+};
+struct TestMyClass : testing::Test
 {
-    EXPECT_EQ(true,true);
+    MyClass *mc;
+    void SetUp(){mc =new MyClass(100);}
+    void TearDown(){delete mc;}
+};
+TEST_F(TestMyClass, Increment_by_5)
+{
+    //act
+    mc->increment(5);
+    //assert
+    ASSERT_EQ(mc->getValue(),105);
+}
+TEST(test_suite1,test1)
+{
+    //Arange
+    int value=100;
+    int increment=5;
+    //act
+    value=value+increment;
+    //assert
+    ASSERT_EQ(value,105);
+}
+int main(int argc, char**argv)
+{
+    testing::InitGoogleTest(&argc,argv);
+    return RUN_ALL_TESTS();
 }
